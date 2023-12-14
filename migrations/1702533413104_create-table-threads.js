9452901f2c39438dcb24764d7076f65ceda37735
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-12-14 13:00:18 +0700

    migrations for threads table

---
+++ migrations/1702533413104_create-table-threads.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'timestamptz',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint(
+    'threads',
+    'fk_threads.owner_users.id',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE'
+  );
+};
+
+exports.down = (pgm) => {};
